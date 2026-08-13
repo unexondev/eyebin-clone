d0b815a037dc66fcf9f6b751e2d1ced782e71770
Ahmet Celikbas <ahmetcelikbas333333@gmail.com>
2026-08-13 16:58:12 +0300

    MaskFactory working fine & can be used from now on. - tests will also remain for a while in factory.py, when it's ensured that everything works fine, then they'll be removed.

--- core/eyebin/training/factory.py
+++ core/eyebin/training/factory.py
@@ -5,6 +5,8 @@ from numpy.typing import NDArray
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
+from dataclasses import dataclass
+
 import pathlib
 
 path = pathlib.Path(__file__)
@@ -13,23 +15,46 @@ dir_ckpts = dir_prj.joinpath("checkpoints/")
 dir_cfgs = dir_prj.joinpath("configs/")
 
 
+@dataclass
+class MaskFactoryOptions:
+    refine_iteratively : bool = True
+    """
+    TODO
+    """
+
+
 PointCoords = list[tuple[int, int]]
-
-
+    
 class MaskFactory:
     """
     Produces segmentation masks using a pre-trained SAM-2 model given on initialization.
     """
 
-    def __init__(self, model):
+    def __init__(self, model, options : MaskFactoryOptions, logit_init : NDArray | None = None):
+
         # define the model
         self.model = model
+
+        # define options
+        self.opts = options
+
         # create the predictor object
         self.predictor = SAM2ImagePredictor(self.model)
 
+        # we use this logit for iterative training FIXME better docstring
+        self.logit_max = logit_init
+
+        self.mask = None
+
 
     @classmethod
-    def create(cls, ckpt_name_or_path : str, cfg_path : str, device : str = None):
+    def create(cls,
+               ckpt_name_or_path : str,
+               cfg_path : str,
+               options : MaskFactoryOptions,
+               device : str = None,
+               logit_init : NDArray | None = None
+               ):
         """
         Craete a MaskFactory instance with a SAM-2 model loaded.
 
@@ -53,14 +78,29 @@ class MaskFactory:
             device=device
             )
         
-        return cls(model=sam2_model)
+        return cls(
+            model=sam2_model,
+            options=options,
+            logit_init=logit_init
+            ) # return a new instance
 
+
+    """
+    Access functions
+    """
+
+    def get(self):
+        return self.mask
+
+
+    """
+    Main functions
+    """
 
     def produce(self,
                 stream_data : NDArray[np.uint8],
                 include_points : PointCoords,
                 exclude_points : PointCoords,
-                previous_mask : NDArray = None,
                 **predict_options
                 ) -> NDArray:
         """
@@ -69,15 +109,16 @@ class MaskFactory:
         TODO better docstring
         """
         prd = self.predictor
+        opts = self.opts
 
         # set image
         prd.set_image(stream_data)
 
         # concat points
-        ndarr_pts = np.concatenate([
-            include_points,
-            exclude_points
-            ]) # [ *include_points, *exclude_points ]
+        ndarr_pts = np.array(include_points + exclude_points)
+
+        if ndarr_pts.size <= 0:
+            return None # no points given
 
         # define labels
         ndarr_labels = np.concatenate([
@@ -89,7 +130,7 @@ class MaskFactory:
         masks, scores, logits = prd.predict(
             point_coords=ndarr_pts,
             point_labels=ndarr_labels,
-            mask_input=previous_mask,
+            mask_input=self.logit_max[None] if self.logit_max is not None else self.logit_max,
             **predict_options # pass options
             )
 
@@ -107,68 +148,75 @@ class MaskFactory:
         idx_best = np.argmax(scores)
 
         # get the max scored mask
-        mask_best = masks[idx_best]
+        mask_max : NDArray[np.float32] = masks[idx_best]
+        self.mask = mask_max.astype(np.bool)
 
-        return mask_best
+        if opts.refine_iteratively:
+            # get the max scored logit
+            self.logit_max = logits[idx_best]
+
+        return True
     
 
 """
 TEST
-"""
-from PIL import Image
-import cv2
+"""    
+def test():
+    from PIL import Image
+    import cv2
 
-maskfactory = MaskFactory.create(
-    ckpt_name_or_path="sam2.1_hiera_tiny.pt",
-    cfg_path="configs/sam2.1/sam2.1_hiera_t.yaml"
-    )
-
-include_coords : PointCoords = []
-exclude_coords : PointCoords = []
-mask_cur : NDArray | None = None
-
-def callback(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        include_coords.append((x, y))
-        
-    elif event == cv2.EVENT_RBUTTONDOWN:
-        exclude_coords.append((x, y))
-
-    
-
-fimage = Image.open("/home/cbsahmet/Dev/eyebin-clone/datasets/IMG-20260505-WA0137_jpg.rf.84f2adf308c8dcd6a4b37783cfaac18d.jpg")
-
-cv2.namedWindow("TEST-MASKFACTORY")
-cv2.setMouseCallback("TEST-MASKFACTORY", callback)
-
-while True:
-
-    image = np.array(fimage)
-
-    view = image
-    if mask_cur is not None:
-        # Maskeyi görüntüye uygula
-        overlay = image.copy()
-        overlay[mask_cur] = (0, 255, 0)
-        view = cv2.addWeighted(
-            image,
-            0.6,
-            overlay,
-            0.4,
-            0
+    maskfactory = MaskFactory.create(
+        ckpt_name_or_path="sam2.1_hiera_tiny.pt",
+        cfg_path="configs/sam2.1/sam2.1_hiera_t.yaml",
+        options=MaskFactoryOptions(
+            refine_iteratively=True
+            )
         )
-    cv2.imshow("TEST-MASKFACTORY", view)
-    
-    key = cv2.waitKey(500)
 
-    if key == 27: # ESC
-        # quit program
-        break
+    include_coords : PointCoords = []
+    exclude_coords : PointCoords = []
 
-    if key == 13: # Enter
-        # perform a prediction
-        mask_cur = maskfactory.produce(image, include_coords, exclude_coords)
-        print(mask_cur.dtype)
-        # then clear previous points
-        include_coords.clear()
-        exclude_coords.clear()
+    def callback(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            include_coords.append((x, y))
+            
+        elif event == cv2.EVENT_MBUTTONDOWN:
+            exclude_coords.append((x, y))
+
+    fimage = Image.open("/home/cbsahmet/Dev/eyebin-clone/datasets/IMG-20260505-WA0137_jpg.rf.84f2adf308c8dcd6a4b37783cfaac18d.jpg")
+
+    cv2.namedWindow("TEST-MASKFACTORY")
+    cv2.setMouseCallback("TEST-MASKFACTORY", callback)
+
+    while True:
+
+        image = np.array(fimage)
+
+        mask_cur = maskfactory.get()
+
+        view = image
+        if mask_cur is not None:
+            # Maskeyi görüntüye uygula
+            overlay = image.copy()
+            overlay[mask_cur] = (0, 255, 0)
+            view = cv2.addWeighted(
+                image,
+                0.6,
+                overlay,
+                0.4,
+                0
+            )
+        cv2.imshow("TEST-MASKFACTORY", view)
+        
+        key = cv2.waitKey(500)
+
+        if key == 27: # ESC
+            # quit program
+            break
+
+        if key == 13: # Enter
+            # perform a prediction
+            maskfactory.produce(image, include_coords, exclude_coords)
+            # then clear previous points
+            include_coords.clear()
+            exclude_coords.clear()
