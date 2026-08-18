5b0a8aee60a71a51efcb4b330599171543925fd8
Ahmet Celikbas <ahmetcelikbas333333@gmail.com>
2026-08-18 16:56:49 +0300

    VideoStreamProfile can be matched with both VideoStreamProfile and pyrealsense2.video_stream_profile.

--- eyebin/core/stream_profile.py
+++ eyebin/core/stream_profile.py
@@ -1,13 +1,19 @@
+from __future__ import annotations
 from dataclasses import dataclass
+
 from pyrealsense2 import stream as StreamType
 from pyrealsense2 import format as StreamFormat
-from pyrealsense2 import video_stream_profile
+from pyrealsense2 import stream_profile, video_stream_profile
 
 
 @dataclass
 class StreamProfile:
+
     stream_type : StreamType
     format : StreamFormat
+
+    def matches(self, other : stream_profile | StreamProfile):
+        raise NotImplementedError()
 
 
 @dataclass
@@ -17,23 +23,21 @@ class VideoStreamProfile(StreamProfile):
     height : int
     fps : int
 
-    def __eq__(self, cmp):
 
-        if isinstance(cmp, VideoStreamProfile):
-            return self.stream_type == cmp.stream_type and \
-                self.format == cmp.format and \
-                self.width == cmp.width and \
-                self.height == cmp.height and \
-                self.fps == cmp.fps
-            
-        elif isinstance(cmp, video_stream_profile):
-            return self.stream_type == cmp.stream_type() and \
-                self.format == cmp.format() and \
-                self.width == cmp.width() and \
-                self.height == cmp.height() and \
-                self.fps == cmp.fps()
-        
-        return NotImplemented
+    def matches(self, other : video_stream_profile | VideoStreamProfile) -> bool:
+
+        if isinstance(other, video_stream_profile):
+            return self.stream_type == other.stream_type() and \
+                self.format == other.format() and \
+                self.width == other.width() and \
+                self.height == other.height() and \
+                self.fps == other.fps()
+
+        if isinstance(other, VideoStreamProfile):
+            return self == other
+
+        raise TypeError("VideoStreamProfile can be matched with an instance of semantically same type.")
+
 
     def __hash__(self):
         return hash((
